3181a5dad6dcd0b220df0e399119041fa49389ea
Md. Mainul Hasan <55666627+Mainulhasan05@users.noreply.github.com>
2025-09-08 20:42:39 +0600

    update

--- src/routes/publicRoutes.js
+++ src/routes/publicRoutes.js
@@ -1,9 +1,11 @@
 const router = require("express").Router();
 const GalleryController = require("../controllers/galleryController");
 const ProductController = require("../controllers/productController");
+const DealerController = require("../controllers/dealerController");
 
 // Public routes
 router.get("/products", ProductController.getProductsPublic);
+router.get("/dealers", DealerController.getDealersPublic);
 router.get("/public/:slug", ProductController.getProductBySlug);
 // Public routes
 router.get("/images", GalleryController.getPublicFeaturedImages);
