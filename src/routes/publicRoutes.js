3809ff441cef0897a422ff83e0f8cf74ab97be3a
Md. Mainul Hasan <55666627+Mainulhasan05@users.noreply.github.com>
2025-09-24 21:17:48 +0600

    feat(routes): add blog routes to public routes
    Add new routes for blog functionality to expose public blog endpoints. Includes routes for getting all blogs and a single blog by slug.

--- src/routes/publicRoutes.js
+++ src/routes/publicRoutes.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const GalleryController = require("../controllers/galleryController");
 const ProductController = require("../controllers/productController");
 const DealerController = require("../controllers/dealerController");
+const BlogController = require("../controllers/blogController");
 
 // Public routes
 router.get("/products", ProductController.getProductsPublic);
@@ -10,5 +11,8 @@ router.get("/public/:slug", ProductController.getProductBySlug);
 // Public routes
 router.get("/images", GalleryController.getPublicFeaturedImages);
 router.get("/gallery/:id", GalleryController.getImageById);
+// Public routes
+router.get("/blogs", BlogController.getBlogsPublic);
+router.get("/blog/:slug", BlogController.getBlogBySlug);
 
 module.exports = router;
