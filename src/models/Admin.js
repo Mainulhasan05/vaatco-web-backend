fef83118446f58e357f139a84916da2f208ca5ec
Md. Mainul Hasan <55666627+Mainulhasan05@users.noreply.github.com>
2025-09-26 11:18:17 +0600

    feat(Admin): add password hashing and comparison methods
    Add pre-save hook to hash passwords before saving and method to compare entered password with stored hash. This enhances security by ensuring passwords are never stored in plaintext.

--- src/models/Admin.js
+++ src/models/Admin.js
@@ -56,4 +56,18 @@ adminSchema.methods.getResetPasswordToken = function () {
 
   return resetToken;
 };
+
+adminSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+// Compare password method
+adminSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 module.exports = mongoose.model("Admin", adminSchema);
